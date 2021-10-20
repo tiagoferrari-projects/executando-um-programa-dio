0a3fd9fae93ab6ef4771cfc363d340be7d100675
tiagoferrari-projects <tiago-projects@outlook.com>
2021-10-19 21:11:34 -0300

    example of using the if and switch/case condition

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -5,7 +5,7 @@ import com.dio.base.Order;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Order order = new Order("code 1234");
+        Order order = new Order("code 1234",99);
 
         System.out.println(order);
 
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -3,15 +3,38 @@ package com.dio.base;
 public class Order {
 
     private final String code;
+    private final int totalValue;
 
-    public Order(String code) {
+    public Order(String code,int totalValue) {
         this.code = code;
+        this.totalValue = totalValue;
     }
+
+    // using switch and case
+    public double calculateFee(){
+        return switch (totalValue) {
+            case 100 -> totalValue * 0.99;
+            case 200 -> totalValue * 1.99;
+            default -> totalValue;
+        };
+    }
+
+    // using if
+    /*
+    public double calculateFee(){
+        if (this.totalValue > 100){
+            return this.totalValue * 0.99;
+        }else {
+            return this.totalValue;
+        }
+    }
+    */
 
     @Override
     public String toString() {
         return "Order{" +
                 "code='" + code + '\'' +
+                ", totalValue=" + totalValue +
                 '}';
     }
 }
